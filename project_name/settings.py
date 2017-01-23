f9ed78019fbe1df48ec795fefbb596d3edb9db3f
Ryan Nowakowski <tubaman@fattuba.com>
2017-01-23 17:16:31 -0600

    Set LOGIN_URL in settings.py
    The LOGIN_URL default is /accounts/login/ which breaks since
    the django-user-accounts app login url is reverse('account_login').

--- project_name/settings.py
+++ project_name/settings.py
@@ -185,6 +185,8 @@ ACCOUNT_LOGOUT_REDIRECT_URL = "home"
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 2
 ACCOUNT_USE_AUTH_AUTHENTICATE = True
 
+LOGIN_URL = "account_login"
+
 AUTHENTICATION_BACKENDS = [
     "account.auth_backends.UsernameAuthenticationBackend",
 ]
